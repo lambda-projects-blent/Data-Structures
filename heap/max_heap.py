531df3f37dd3a30b2089b4524f38ddea3cc515a9
Brandon <Blent1050@yahoo.com>
2019-05-08 17:35:02 -0400

    implemented Heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,27 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    pass
+    def _bubble_up(self, index):
+        while index > 0:
+            parent = (index-1) // 2
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                index = parent
+            else:
+                break
 
-  def _sift_down(self, index):
-    pass
+    def _sift_down(self, index):
+        pass
