ec5bf8331bd1e71a063daea77f0a420c641b5e6e
Brandon <Blent1050@yahoo.com>
2019-05-08 17:33:58 -0400

    Implemented Queue. Re-commiting all changes due to moving repository over to lambda-projects

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,33 @@
+from linked_list import LinkedList
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = LinkedList()
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.add_to_tail(item)
+        return self.storage.tail.get_value()
 
-  def len(self):
-    pass
+    def dequeue(self):
+        return f"{self.storage.head.get_value()} was removed"
+        return self.storage.remove_head()
+
+    def len(self):
+        self.size = 0
+        if not self.storage.head and not self.storage.tail:
+            return 0
+
+        if self.storage.head == self.storage.tail:
+            return 1
+
+        current_value = self.storage.head
+
+        while current_value:
+            self.size += 1
+            current_value = current_value.get_next()
+
+        return self.size
